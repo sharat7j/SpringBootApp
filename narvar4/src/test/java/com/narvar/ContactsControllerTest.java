73d570113480552246f2a5e6b0aa8fbd08ba5254
sharat7j <sharsmail@gmail.com>
2016-08-01 15:32:33 -0700

    Update ContactsControllerTest.java

--- narvar4/src/test/java/com/narvar/ContactsControllerTest.java
+++ narvar4/src/test/java/com/narvar/ContactsControllerTest.java
@@ -1,124 +1,132 @@
 package com.narvar;
 
+import com.google.gson.JsonObject;
+import com.narvar.Narvar4Application;
+import com.narvar.domain.Contacts;
 
-import org.apache.http.HttpStatus;
-import org.hamcrest.Matchers;
-import org.junit.Assert;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.sql.DataSource;
+
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.mock.http.MockHttpOutputMessage;
-import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
-import static com.jayway.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
-
-import org.apache.http.HttpStatus;
-import org.junit.*;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.boot.test.*;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.http.ContentType;
-import com.narvar.domain.ContactRepository;
-import com.narvar.domain.Contacts;
-
-import org.hamcrest.Matchers.*;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Narvar4Application.class)
+@ActiveProfiles("postgres")
 @WebAppConfiguration
-@IntegrationTest("server.port:0") 
-public class ContactsControllerTest {
-    private HttpMessageConverter mappingJackson2HttpMessageConverter;
-    private MockMvc mockMvc;
-    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-            MediaType.APPLICATION_JSON.getSubtype(),
-            Charset.forName("utf8"));
-    private List<Contacts> contactsList = new ArrayList<>();
-    @Autowired
-    private ContactRepository contactRepository;
-    
-    private String name1 = "test1";
-    private String email1="test1@gmail.com";
-    private String profession1="scientist";
-    
-    private String name2 = "test2";
-    private String email2="test2@gmail.com";
-    private String profession2="lawyer";
-    private Contacts contact;
-    @Value("${local.server.port}")
-    int port;
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    @Autowired
-    void setConverters(HttpMessageConverter<?>[] converters) {
-
-        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream().filter(
-                hmc -> hmc instanceof MappingJackson2HttpMessageConverter).findAny().get();
-
-        Assert.assertNotNull("the JSON message converter must not be null",
-                this.mappingJackson2HttpMessageConverter);
-    }
-    
-    @Before
-    public void setup() throws Exception {
-
-        this.contactRepository.deleteAll();;
-
-        this.contact = contactRepository.save(new Contacts(name1,email1,profession1));
-        contactsList.add(new Contacts(name1,email1,profession1));
-        contactsList.add(new Contacts(name2,email2,profession2));
-        RestAssured.port = port;
-    }
-
-  
-    
-    @Test
-    public void canFetchAll() {
-        when().
-                get("/contactlist").
-        then().
-                statusCode(HttpStatus.SC_OK).
-                body("name",hasItems("test1","test2"));
-    }
+@IntegrationTest
+@SpringApplicationConfiguration(classes = Narvar4Application.class)
+public class ControllerTest {
 	
+	
+	@Autowired
+	DataSource dataSource;
 
-    
-    
-	protected String json(Object o) throws IOException {
-        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
-        this.mappingJackson2HttpMessageConverter.write(
-                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
-        return mockHttpOutputMessage.getBodyAsString();
-    }
+	RestTemplate restTemplate;
+
+
+	@Before
+	public void setUp() {
+		restTemplate = new TestRestTemplate("user", "secret");
+
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		jdbcTemplate.execute("TRUNCATE TABLE contacts");
+		jdbcTemplate.execute("INSERT INTO contacts VALUES (1, 'test1','test1@gmail.com','prof1')");
+		jdbcTemplate.execute("INSERT INTO contacts VALUES (2, 'test2','test2@gmail.com','prof2')");
+		
+	}
+	
+	@Test
+	public void shouldGetContacts() throws Exception {
+		ResponseEntity<Map> responseEntity = restTemplate
+				.getForEntity("http://localhost:8080/contactlist", Map.class);
+
+		assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));
+		assertThat(responseEntity.getHeaders().getContentType(), is(MediaType.APPLICATION_JSON_UTF8));
+
+		Map<String, Map<String,List<Object>>> account = responseEntity.getBody();
+		assertEquals(account.get("_embedded").get("contacts").size(),2);
+	}
+
+	
+	@Test
+	public void shouldCreateContacts() throws Exception {
+		
+		// create payload
+		RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:8080/contacts/createUser";
+        Map<String, String> map = new HashMap<String, String>();
+        restTemplate.setMessageConverters(Arrays.asList(new StringHttpMessageConverter(), new FormHttpMessageConverter()));
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+        params.set("name", "test3");
+        params.set("email", "test3@gmail.com");
+        params.set("profession", "prof3");
+        ResponseEntity<String> entity= restTemplate.postForEntity(url, params,String.class);
+        JSONObject resp=new JSONObject(entity.getBody());
+        assertEquals(resp.get("name"), "test3");
+        
+	}
+
+	@Test
+	public void shouldUpdateContacts() throws Exception {
+		
+		// create payload
+		RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:8080/contacts/updateUser";
+        Map<String, String> map = new HashMap<String, String>();
+        restTemplate.setMessageConverters(Arrays.asList(new StringHttpMessageConverter(), new FormHttpMessageConverter()));
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+        params.set("name", "test2");
+        params.set("email", "test3@gmail.com");
+        params.set("profession", "prof4");
+        ResponseEntity<String> entity= restTemplate.postForEntity(url, params,String.class);
+        JSONObject resp=new JSONObject(entity.getBody());
+        assertEquals(resp.get("profession"), "prof4");
+        
+	}
+	
+	
+	@Test
+	public void shouldDeleteContacts() throws Exception {
+		
+		ResponseEntity<Map> responseEntity = restTemplate
+				.exchange("http://localhost:8080/contacts/{name}/deleteUser",
+						HttpMethod.DELETE, null, Map.class, "test2");
+
+		assertThat(responseEntity.getStatusCode(), is(HttpStatus.NO_CONTENT));
+		assertThat(responseEntity.getBody(), nullValue());
+        
+	}
+	
+	
 	
 }
